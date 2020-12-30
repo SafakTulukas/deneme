d96b391c873c53f41e925c9bbf5904b9a1b70e51
SafakTulukas <safaktulukas@hotmail.com>
2020-12-30 20:50:53 +0300

    End Of Section 5

--- API/Controllers/ProductsController.cs
+++ API/Controllers/ProductsController.cs
@@ -2,19 +2,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using API.Dtos;
+using API.Errors;
 using AutoMapper;
 using Core.Entities;
 using Core.Interfaces;
 using Core.Specifications;
 using Infrastructure.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Controllers
-{
-    [ApiController]
-    [Route("api/products")]
-    public class ProductsController : ControllerBase
+{    
+    public class ProductsController : BaseApiController
     {
         private readonly IGenericRepository<Product> _productRepo;
         private readonly IGenericRepository<ProductBrand> _brandRepo;
@@ -36,16 +36,20 @@ namespace API.Controllers
             var spec = new ProductsWithTypesAndBrandsSpecification();
 
             var products = await _productRepo.ListAsync(spec);
-
+            
             return Ok(_mapper.Map<IReadOnlyList<ProductToReturnDto>>(products));
         }
 
         [HttpGet("{id}")]
+        [ProducesResponseType(StatusCodes.Status200OK)] // Swagger da 200 durum bilgisini göster
+        [ProducesResponseType(typeof(ApiResponse),StatusCodes.Status404NotFound)] // Swagger da 404 durum bilgisini göster
         public async Task<ActionResult<ProductToReturnDto>> GetProduct(int id)
         {
             var spec = new ProductsWithTypesAndBrandsSpecification(id);
 
             var product = await _productRepo.GetEntityWithSpec(spec);
+
+            if(product ==null) return NotFound(new ApiResponse(404));
 
             return Ok(_mapper.Map<ProductToReturnDto>(product));
         }
--- API/Startup.cs
+++ API/Startup.cs
@@ -1,21 +1,14 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using API.Extensions;
 using API.Helpers;
+using API.Middleware;
 using AutoMapper;
-using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using Microsoft.OpenApi.Models;
 
 namespace API
 {
@@ -26,35 +19,31 @@ namespace API
         public Startup(IConfiguration config)
         {
             _config = config;
-        }        
-
-        
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddScoped<IProductRepository,ProductRepository>();
-            services.AddScoped(typeof(IGenericRepository<>),typeof(GenericRepository<>));
-            services.AddAutoMapper(typeof(MappingProfiles));
-
-            services.AddControllers();
-            
-            services.AddDbContext<StoreContext>(options =>{
-                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
-            });
-
-            services.AddSwaggerGen(c =>
-            {
-                c.SwaggerDoc("v1", new OpenApiInfo { Title = "API", Version = "v1" });
-            });
         }
 
-      
+
+        public void ConfigureServices(IServiceCollection services)
+        {            
+            services.AddAutoMapper(typeof(MappingProfiles));
+            services.AddControllers();
+            services.AddDbContext<StoreContext>(options =>
+            {
+                options.UseSqlServer(_config.GetConnectionString("DefaultConnection"));
+            });
+            services.AddApplicationServices();
+            services.AddSwaggerDocumentation();           
+        }
+
+
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            app.UseMiddleware<ExceptionMiddleware>();
+            app.UseStatusCodePagesWithReExecute("/errors/{0}");
+
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
-                app.UseSwagger();
-                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "API v1"));
+                //app.UseDeveloperExceptionPage();
+               app.UseSwaggerDocumention();
             }
 
             app.UseHttpsRedirection();
